be1783ab6857c44ca27d3c5da380ded3e56cc7dd
groditi <groditi@gmail.com>
2021-10-18 12:35:43 -0700

    multiple little big fixes and doc updates

--- R/get_series_table.R
+++ R/get_series_table.R
@@ -8,12 +8,14 @@
 #' @param ... additional parameters to pass to [`bls_request`]
 #'
 #' @return a tibble of observations
+#'
+#' @family <blsR-requests>
+#'
 #' @export
 #'
-#' @examples
+
 get_series_table <- function(series_id, start_year=NA, end_year=NA, ...){
-  results <- bls_request(query_series(series_id, ...))
-  data_as_table(results$series$data)
+  data_as_table(get_series(series_id, start_year, end_year, ...)$data)
 }
 
 
@@ -28,9 +30,12 @@ get_series_table <- function(series_id, start_year=NA, end_year=NA, ...){
 #' @param ... additional parameters to pass to [`query_n_series`]
 #'
 #' @return a list of tables
+#'
+#' @family <blsR-requests>
+#'
 #' @export
 #'
-#' @examples
+
 get_series_tables <- function(series_ids, api_key, start_year=NA, end_year=NA, ...){
   lapply(
     get_n_series(series_ids, api_key, start_year=NA, end_year=NA, ...),
@@ -47,14 +52,20 @@ get_series_tables <- function(series_ids, api_key, start_year=NA, end_year=NA, .
 #'  <https://data.bls.gov/registrationEngine/>
 #' @param start_year optional numeric 4-digit year
 #' @param end_year optional numeric 4-digit year
+#' @param tidy optional boolean. Return will use [`tidy_periods()`] if true
 #' @param ... additional parameters to pass to [`query_n_series`]
 #'
-#' @return
+#' @return a tibble of multiple merged time series
+#'
+#' @family <blsR-requests>
+#'
 #' @export
 #'
 #' @examples
-get_n_series_table <- function(series_ids, api_key, start_year=NA, end_year=NA, tidy=TRUE, ...){
-  tidy_periods(merge_tables(
+get_n_series_table <- function(series_ids, api_key, start_year=NA, end_year=NA, tidy=FALSE, ...){
+  table <- merge_tables(
     get_series_tables(series_ids, api_key, start_year=NA, end_year=NA, ...)
-  ) )
+  )
+  if(tidy) return(tidy_periods(table))
+  return(table)
 }
