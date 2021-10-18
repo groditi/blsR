b5f01a4f9193f1c1aef9f511b74477e3349f13ea
groditi <groditi@gmail.com>
2021-10-17 17:25:58 -0700

    I changed my mind on the API, eliminate the retriever closure.

--- R/blsR.R
+++ R/blsR.R
@@ -1,12 +1,86 @@
-#' Retrieve Data From the U.S. Bureau Of Labor Statistics API v2
+#' blsR: Retrieve Data From the U.S. Bureau Of Labor Statistics API
+#'
+
+#' `blsR` provides functions for retrieving and processing data from the BLS API.
+#'  The functions are divided into 4 categories: query generators, query requests,
+#'  result processors, and the user-friendly simplified interface.
+#'
+#'  @section Query generators:
+#'
+#'  The query generators return a list suitable for passing to [`bls_request()`].
+#'  Most users should never need to access these functions directly but they are
+#'  made available for advanced users and user-extensions.
+#'
+#'  * [`query_series()`] - Create a query for a single time series
+#'  * [`query_n_series()`] - Create a query to retrieve one or more time series
+#'   and their catalog data
+#'  * [`query_popular_series()`] - Create a query to retrieve popular series
+#'  * [`query_all_surveys()`] - Create a query to retrieve all surveys
+#'  * [`query_survey_info()`] - Create a query to retrieve information about a
+#'   survey
+#'  * [`query_latest_observation()`] - Create a Query to retrieve the latest
+#'  observation for a time series
+#'
+#'  @section Query Requests:
+#'
+#'  The query-requester functions will execute the query by making the API request
+#'  and returning a minimally-processed response. These are likely to be the
+#'  most suitable functions to use for users who want to access the raw results.
+#'
+#'  * [`bls_request()`] - Execute a query and return the unprocessed results
+#'  * [`get_series()`] - Create and execute query for a single time series
+#'  * [`get_n_series()`] - Create and execute a query to retrieve one or more
+#'  time series and their catalog data
+#'  * [`get_popular_series()`] - Create and execute a query to retrieve popular
+#'  series
+#'  * [`get_all_surveys()`] - Create and execute a query to retrieve all surveys
+#'  * [`get_survey_info()`] - Create and execute a query to retrieve information
+#'  about a survey
+#'  * [`get_latest_observation()`] - Create and execute a query to retrieve the
+#'  latest observation for a time series
+#'
+#'  @section Result Processors:
+#'
+#'  The result-processor functions will transform the raw API response data
+#'  structures into data structures more likely to be suitable for modern user
+#'  workflows. The functions generally take as input the values returned by the
+#'  query-requester functions and make transform the data to different formats
+#'  or modify the output of another result-processor function.
+#'
+#'  * [`data_as_table()`] - Flatten the data list into a table
+#'  * [`merge_tables()`] - Merge multiple tables by period
+#'  * [`tidy_periods()`] - Transform periods to a more useful format
+#'  * [`data_as_tidy_table()`] - Flatten the data list and transform period data
+#'  * [`merge_tidy_tables()`] - Merge multiple tables with tidy period data
+#'
+#'  @section Simplified Interface:
+#'
+#'  These functions simplify the query generation, execution, and response
+#'  processing into a single function call. For most common use cases these are
+#'  likely to be the only functions needed.
+#'
+#'  * [`get_series_table()`] - Request one series and return a data table
+#'  * [`get_series_tables()`] - Request series and return list of data tables
+#'  * [`get_n_series_table()`] - Request series and return one table of values
+#'
+#'  @docType package
+#'  @name blsR
+NULL
+#>  NULL
+
+
+
+
+
 #'
 #' @description
 #'
-#' `blsR()` constructs a function that will execute queries against the BLS API.
+#' `bls_request()` constructs a function that will execute queries against the BLS API.
 #' Queries are generated using one of the query-generating functions and then
 #' passed on to the retriever function returned by `blsR()`. The retriever
 #' function will return the requested data.
 #'
+#' @param query
 #' @param api_key string, only necessary for retrieving multiple series in one
 #' request, requesting calculations, or custom time frames and catalog data
 #' @param user_agent string, optional
